bf455e132b1906a19fb2e5422176d5765771d20a
fjokery <yemoisscared@gmail.com>
2024-04-05 14:40:49 +0200

    slideshow visas

--- projektASP/projektASP/Controllers/HomeController.cs
+++ projektASP/projektASP/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace projektASP.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
